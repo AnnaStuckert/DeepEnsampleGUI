f01ec126bc5067b3daceac4cf4917f5f0785fe87
AnnaStuckert <47814177+AnnaStuckert@users.noreply.github.com>
2025-03-26 17:43:34 +0000

    think everything is updated that should update the plug-in, but unsure how to make the plug-in actually use it

--- napari-threshold/src/napari_threshold/_tests/test_widget.py
+++ napari-threshold/src/napari_threshold/_tests/test_widget.py
@@ -1,10 +1,14 @@
 import numpy as np
-
-from napari_thresholds import ExampleQWidget, example_magic_widget, threshold_f, leaf_area
-
 import pytest
-from napari.types import ImageData, LabelsData
 from napari.layers import Image, Labels
+from napari.types import ImageData, LabelsData
+from napari_thresholds import (
+    ExampleQWidget,
+    example_magic_widget,
+    leaf_area,
+    threshold_f,
+)
+
 
 # make_napari_viewer is a pytest fixture that returns a napari viewer object
 # capsys is a pytest fixture that captures stdout and stderr output streams
@@ -38,31 +42,37 @@ def test_example_magic_widget(make_napari_viewer, capsys):
     captured = capsys.readouterr()
     assert captured.out == f"you have selected {layer}\n"
 
+
 @pytest.fixture
 def im_rgb():
-    return ImageData(np.random.randint(256,size=(256,256,3)))
+    return ImageData(np.random.randint(256, size=(256, 256, 3)))
+
 
 @pytest.fixture
 def mask_bin():
-    return Labels(np.random.randint(2,size=(256,256,1)))
+    return Labels(np.random.randint(2, size=(256, 256, 1)))
+
 
 def get_er(*args, **kwargs):
     er_func = threshold_f()
     return er_func(*args, **kwargs)
 
+
 def test_threshold(im_rgb):
-    my_widget_thd = get_er(im_rgb,filter_selected='otsu')
-    #check if output is numpy array
-    assert type(my_widget_thd)==np.ndarray
-    #check if output is binary
-    assert len(np.unique(my_widget_thd))==2
+    my_widget_thd = get_er(im_rgb, filter_selected="otsu")
+    # check if output is numpy array
+    assert type(my_widget_thd) == np.ndarray
+    # check if output is binary
+    assert len(np.unique(my_widget_thd)) == 2
+
 
 def get_ed(*args, **kwargs):
     ed_func = leaf_area()
     return ed_func(*args, **kwargs)
-    
+
+
 def test_leaf_area(mask_bin):
     my_widget_lf_area = get_ed(mask_bin)
-    #check if output is numpy array
-    assert isinstance(my_widget_lf_area,int)
+    # check if output is numpy array
+    assert isinstance(my_widget_lf_area, int)
     assert my_widget_lf_area > 0
--- napari-threshold/src/napari_threshold/models.py
+++ napari-threshold/src/napari_threshold/models.py
@@ -1,27 +1,50 @@
 import torch
 import torch.nn as nn
 
+
 class UNet(nn.Module):
     def __init__(self, in_f=1, out_f=1, dropout_rate=0.7):
         super(UNet, self).__init__()
-        
+
         # Encoder
-        self.enc_conv1 = nn.Sequential(nn.Conv2d(in_f, 64, kernel_size=3, padding=1), nn.ReLU(inplace=True))
-        self.enc_conv2 = nn.Sequential(nn.Conv2d(64, 128, kernel_size=3, padding=1), nn.ReLU(inplace=True))
-        self.enc_conv3 = nn.Sequential(nn.Conv2d(128, 256, kernel_size=3, padding=1), nn.ReLU(inplace=True))
+        self.enc_conv1 = nn.Sequential(
+            nn.Conv2d(in_f, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+        )
+        self.enc_conv2 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, padding=1), nn.ReLU(inplace=True)
+        )
+        self.enc_conv3 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+        )
 
         self.pool = nn.MaxPool2d(2, 2)
         self.dropout = nn.Dropout(dropout_rate)
 
         # Bottleneck
-        self.bottleneck_conv = nn.Sequential(nn.Conv2d(256, 512, kernel_size=3, padding=1), nn.ReLU(inplace=True))
+        self.bottleneck_conv = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+        )
 
         # Decoder
-        self.dec_conv3 = nn.Sequential(nn.Conv2d(512 + 256, 256, kernel_size=3, padding=1), nn.ReLU(inplace=True))
-        self.dec_conv2 = nn.Sequential(nn.Conv2d(256 + 128, 128, kernel_size=3, padding=1), nn.ReLU(inplace=True))
-        self.dec_conv1 = nn.Sequential(nn.Conv2d(128 + 64, 64, kernel_size=3, padding=1), nn.ReLU(inplace=True))
+        self.dec_conv3 = nn.Sequential(
+            nn.Conv2d(512 + 256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+        )
+        self.dec_conv2 = nn.Sequential(
+            nn.Conv2d(256 + 128, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+        )
+        self.dec_conv1 = nn.Sequential(
+            nn.Conv2d(128 + 64, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+        )
 
-        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.upsample = nn.Upsample(
+            scale_factor=2, mode="bilinear", align_corners=True
+        )
 
         # Final Output
         self.final_conv = nn.Conv2d(64, out_f, kernel_size=1)
@@ -59,3 +82,123 @@ class UNet(nn.Module):
         # Final Output
         x = self.final_conv(x)
         return x
+
+
+class convBlock(nn.Module):
+    def __init__(
+        self,
+        inCh,
+        nhid,
+        nOp,
+        twod=True,
+        pool=True,
+        ker=3,
+        padding=1,
+        pooling=2,
+    ):
+        super(convBlock, self).__init__()
+
+        if twod:
+            self.enc1 = nn.Conv2d(inCh, nhid, kernel_size=ker, padding=1)
+            self.enc2 = nn.Conv2d(nhid, nOp, kernel_size=ker, padding=1)
+            self.bn = nn.BatchNorm2d(inCh)
+
+            if pool:
+                self.scale = nn.AvgPool2d(kernel_size=pooling)
+            else:
+                self.scale = nn.Upsample(scale_factor=pooling)
+        else:
+            self.enc1 = nn.Conv3d(inCh, nhid, kernel_size=ker, padding=1)
+            self.enc2 = nn.Conv3d(nhid, nOp, kernel_size=ker, padding=1)
+            self.bn = nn.BatchNorm3d(inCh)
+
+            if pool:
+                self.scale = nn.AvgPool3d(kernel_size=pooling)
+            else:
+                self.scale = nn.Upsample(scale_factor=pooling)
+
+        self.pool = pool
+        self.act = nn.ReLU()
+
+    def forward(self, x):
+        x = self.scale(x)
+        x = self.bn(x)
+        x = self.act(self.enc1(x))
+        x = self.act(self.enc2(x))
+        return x
+
+
+import pdb
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.functional import sigmoid, softmax, softplus
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class Unet_mouse(nn.Module):
+    def __init__(self, nhid=8, ker=3, inCh=1, _ h=224, w=224):
+        super(Unet_mouse, self).__init__()
+        ### U-net Encoder with 3 downsampling layers
+        self.uEnc11 = nn.Conv2d(inCh, nhid, kernel_size=ker, padding=1)
+        self.uEnc12 = nn.Conv2d(nhid, nhid, kernel_size=ker, padding=1)
+
+        self.uEnc2 = convBlock(nhid, 2 * nhid, 2 * nhid, pool=True)
+        self.uEnc3 = convBlock(2 * nhid, 4 * nhid, 4 * nhid, pool=True)
+        # self.uEnc4 = convBlock(4*nhid,8*nhid,8*nhid,pool=True)
+        # self.uEnc5 = convBlock(8*nhid,16*nhid,16*nhid,pool=True)
+
+        ### U-net decoder
+        # self.dec5 = convBlock(16*nhid,8*nhid,8*nhid,pool=False)
+        # self.dec4 = convBlock(16*nhid,4*nhid,4*nhid,pool=False)
+        self.dec3 = convBlock(
+            4 * nhid, 2 * nhid, 2 * nhid, pool=False, pooling=2
+        )
+        self.dec2 = convBlock(4 * nhid, nhid, nhid, pool=False, pooling=2)
+
+        self.dec11 = nn.Conv2d(2 * nhid, nhid, kernel_size=ker, padding=1)
+        self.dec12 = nn.Conv2d(nhid, 1, kernel_size=ker, padding=1)
+
+        self.act = nn.ReLU()
+
+        self.h = h
+        self.w = w
+
+    def encoder(self, x_in):
+        ### Unet Encoder
+        x = []
+
+        x.append(self.act(self.uEnc12(self.act(self.uEnc11(x_in)))))
+        x.append(self.uEnc2(x[-1]))
+        x.append(self.uEnc3(x[-1]))
+        # x.append(self.uEnc4(x[-1]))
+        # x.append(self.uEnc5(x[-1]))
+
+        return x
+
+    def decoder(self, x_enc):
+
+        x = self.dec3(x_enc[-1])
+        x = torch.cat((x, x_enc[-2]), dim=1)
+        x = self.dec2(x)
+        x = torch.cat((x, x_enc[-3]), dim=1)
+        #        x = self.dec3(x_en[])
+        #        x = torch.cat((x,x_enc[-4]),dim=1)
+        #        x = self.dec2(x)
+        #        x = torch.cat((x,x_enc[-5]),dim=1)
+        fmap = self.act(self.dec11(x))
+        x = self.dec12(fmap)
+
+        return x, fmap
+
+    def forward(self, x):
+        #        pdb.set_trace()
+        # Unet encoder result
+        x_enc = self.encoder(x)
+        # Outputs for MSE
+        xHat, fmap = self.decoder(x_enc)
+
+        return xHat, fmap
